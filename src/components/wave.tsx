7d551740489bc7b1a0533ffbbd3e09ab16c6bccd
Miguel Cardoso <6021036+mncardoso@users.noreply.github.com>
2024-01-07 20:45:14 +0000

    fix: 🐛 wave fix

--- src/components/wave.tsx
+++ src/components/wave.tsx
@@ -1,78 +1,80 @@
-// import dynamic from 'next/dynamic';
+import p5 from 'p5';
+import { type Sketch } from '@p5-wrapper/react';
+import { NextReactP5Wrapper } from '@p5-wrapper/next';
 
-// let Sketch = dynamic(() => import('react-p5').then(mod => mod.default), {
-//   ssr: false,
-// });
+let increment = 0.2;
+let scale = 30;
+let sculpt = 40;
+let columns: number;
+let rows: number;
+let width: number;
+let height: number;
+let canvas;
+let flying = 0;
+let terrain: (number | undefined)[][] = [];
 
-// let inc = 0.2;
-// let scl = 30;
-// let scltp = 40;
-// let cols, rows, w, h, canvas;
-// let flying = 0;
-// let terrain = [];
+const Wave: Sketch = (p: p5) => {
+  p.setup = () => {
+    width = window.innerWidth * 2;
+    height = window.innerHeight * 0.75;
+    canvas = p.createCanvas(window.innerWidth, window.innerHeight, p.WEBGL);
+    p.angleMode(p.DEGREES);
+    p.colorMode(p.HSL);
+    columns = width / scale;
+    rows = height / scale;
+    p.frameRate(30);
+    // }
+  };
 
-// const Wave = () => {
-//   const setup = (p5, canvasParentRef) => {
-//     w = window.innerWidth * 2;
-//     h = window.innerHeight * 0.75;
-//     canvas = p5.createCanvas(window.innerWidth, window.innerHeight, p5.WEBGL).parent(canvasParentRef);
-//     p5.angleMode(p5.DEGREES);
-//     p5.colorMode(p5.HSL);
-//     cols = w / scl;
-//     rows = h / scl;
-//     p5.frameRate(30);
-//   };
+  p.draw = () => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      p.background(0, 0, 10, 1);
+    } else {
+      p.background(0, 0, 90, 1);
+    }
+    flying -= 0.02;
+    let yOffset = 0;
+    for (let x = 0; x < columns; x++) {
+      terrain[x] = [] as number[];
+      let xOffset = flying;
+      for (let y = 0; y < rows; y++) {
+        let drawX = terrain[x];
+        if (!drawX) continue;
+        drawX[y] = p.map(p.noise(xOffset, yOffset), 0, 1, -sculpt, sculpt);
+        terrain[x] = drawX;
+        xOffset += increment;
+      }
+      yOffset += increment;
+    }
+    p.translate(0, 120);
+    p.rotateX(90);
+    p.translate(-width / 2, 0);
+    for (let y = 0; y < rows - 1; y++) {
+      p.beginShape(p.TRIANGLE_STRIP);
+      for (let x = 0; x < columns; x++) {
+        p.smooth();
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          p.fill(0, 0, 10, 1);
+          p.stroke(0, 0, 90, 1);
+          // p.stroke(195, 90, p.map(x, 0, columns, 0, 62));
+        } else {
+          p.fill(0, 0, 90, 1);
+          p.stroke(0, 0, 10, 1);
+          // p.stroke(195, 90, p.map(x, 0, columns, 0, 62));
+        }
+        p.strokeWeight(0.5);
+        let readX = terrain[x];
+        if (!readX) continue;
+        p.vertex(x * scale, y * scale, readX[y]);
+        p.vertex(x * scale, (y + 1) * scale, readX[y + 1]);
+      }
+      p.endShape();
+    }
+    window.addEventListener('resize', p.resizeCanvas(window.innerWidth, window.innerHeight) as any);
+  };
+};
+const Canvas = () => {
+  return <NextReactP5Wrapper sketch={Wave} />;
+};
 
-//   const draw = p5 => {
-//     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//       p5.background(0, 0, 10, 1);
-//     } else {
-//       p5.background(0, 0, 90, 1);
-//     }
-//     flying -= 0.02;
-//     let yoff = 0;
-//     for (let x = 0; x < cols; x++) {
-//       terrain[x] = [];
-//       let xoff = flying;
-//       for (let y = 0; y < rows; y++) {
-//         terrain[x][y] = p5.map(p5.noise(xoff, yoff), 0, 1, -scltp, scltp);
-
-//         xoff += inc;
-//       }
-//       yoff += inc;
-//     }
-
-//     p5.translate(0, 120);
-//     p5.rotateX(90);
-//     p5.translate(-w / 2, 0);
-//     for (let y = 0; y < rows - 1; y++) {
-//       p5.beginShape(p5.TRIANGLE_STRIP);
-//       for (let x = 0; x < cols; x++) {
-//         p5.smooth();
-//         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//           p5.fill(0, 0, 10, 1);
-//           p5.stroke(0, 0, 90, 1);
-//           // p5.stroke(195, 90, p5.map(x, 0, cols, 0, 62));
-//         } else {
-//           p5.fill(0, 0, 90, 1);
-//           p5.stroke(0, 0, 10, 1);
-//           // p5.stroke(195, 90, p5.map(x, 0, cols, 0, 62));
-//         }
-//         p5.strokeWeight(0.5);
-//         p5.vertex(x * scl, y * scl, terrain[x][y]);
-//         p5.vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
-//       }
-//       p5.endShape();
-//     }
-//     window.addEventListener('resize', p5.resizeCanvas(window.innerWidth, window.innerHeight));
-//   };
-
-//   return (
-//     <div className="background" id="background">
-//       <Sketch setup={setup} draw={draw} />
-//     </div>
-//   );
-// };
-// // windowResized={windowResized}
-
-// export default Wave;
+export default Canvas;
